322bbd1b2df20643410e0d2a2a4a63160a718b01
devinalzate <98599273+devinalzate@users.noreply.github.com>
2024-02-14 00:00:55 -0500

    Update funciones.js

--- funciones.js
+++ funciones.js
@@ -1,9 +1,14 @@
-
+var n1;
+var a1;
+var n2;
+var a2;
+var n3;
+var a3;
 function asignarNombres(){
-    var n1 = document.getElementById('nombre1').value;
-    var a1 = document.getElementById('apellido1').value;
-    var n2 = document.getElementById('nombre2').value;
-    var a2 = document.getElementById('apellido2').value;
-    var n3 = document.getElementById('nombre3').value;
-    var a3 = document.getElementById('apellido3').value;
+    n1 = document.getElementById('nombre1').value;
+    a1 = document.getElementById('apellido1').value;
+    n2 = document.getElementById('nombre2').value;
+    a2 = document.getElementById('apellido2').value;
+    n3 = document.getElementById('nombre3').value;
+    a3 = document.getElementById('apellido3').value;
 }
