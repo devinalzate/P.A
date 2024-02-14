f6d7eac0c0b039c7dda2cdaec1d90490344860fb
devinalzate <98599273+devinalzate@users.noreply.github.com>
2024-02-13 23:56:03 -0500

    Update funciones.js

--- funciones.js
+++ funciones.js
@@ -1,5 +1,9 @@
 
 function asignarNombres(){
     var n1 = document.getElementById('nombre1').value;
-    alert(n1);
+    var a1 = document.getElementById('apellido1').value;
+    var n2 = document.getElementById('nombre2').value;
+    var a2 = document.getElementById('apellido2').value;
+    var n3 = document.getElementById('nombre3').value;
+    var a3 = document.getElementById('apellido3').value;
 }
