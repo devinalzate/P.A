a5c4f9bcdce53d7a44962385cb7169f546f10df6
devinalzate <98599273+devinalzate@users.noreply.github.com>
2024-02-15 22:19:48 -0500

    Add files via upload

--- funciones.js
+++ funciones.js
@@ -1,19 +1,24 @@
-var n1;
-var a1;
-var n2;
-var a2;
-var n3;
-var a3;
-function asignarNombres(){
-    n1 = document.getElementById('nombre1').value;
-    a1 = document.getElementById('apellido1').value;
-    n2 = document.getElementById('nombre2').value;
-    a2 = document.getElementById('apellido2').value;
-    n3 = document.getElementById('nombre3').value;
-    a3 = document.getElementById('apellido3').value;
+function getVarsUrl(){
+    var url= location.search.replace("?", "");
+    var arrUrl = url.split("&");
+    var urlObj={};
+    var variable = ""; 
+    var valor = ""; 
+    for(var i=0; i<arrUrl.length; i++){
+    var x= arrUrl[i].split("=");
+    variable = x[0]; 
+    valor = x[1]; 
+	urlObj[variable] = valor; 
+    //urlObj[x[0]]=x[1]
+    }
+    return urlObj;
 }
-
-function escribirNombres() {
-    alert(a1);
-    alert(n3);
+function Escribir(){
+    var misVariablesGet = getVarsUrl(); 
+    document.getElementById("nombreja1").innerHTML=misVariablesGet["nombre1"];
+    document.getElementById("apellidoja1").innerHTML=misVariablesGet["apellido1"];
+    document.getElementById("nombreja2").innerHTML=misVariablesGet["nombre2"];
+    document.getElementById("apellidoja2").innerHTML=misVariablesGet["apellido2"];
+    document.getElementById("nombreja3").innerHTML=misVariablesGet["nombre3"];
+    document.getElementById("apellidoja3").innerHTML=misVariablesGet["apellido3"];
 }
